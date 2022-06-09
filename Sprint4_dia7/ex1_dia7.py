13906ca287b9a0a8d94fc316f0a1635c19e5046b
Rodrigo-Alves-da-Silva <106790454+Rodrigo-Alves-da-Silva@users.noreply.github.com>
2022-06-09 00:01:52 -0300

    exercício 1 dia 7

---
+++ Sprint4_dia7/ex1_dia7.py
@@ -0,0 +1,6 @@
+# exercício 1 dia 7 - Baixe o arquivo do link JSON 1, abra ele no vsCode com Python nomeando-o como partida guarde em uma variável e printe o JSON inteiro no terminal.
+import json
+
+with open("GxdV3pRP.json", encoding="utf-8") as partida:
+    exibir = json.load(partida)
+    print(exibir)
